6a98bfc8c4b13a9f86f384cdfbbf187de7114de8
Reasy <nitishkumarps997@gmail.com>
2020-10-19 01:12:49 +0530

    navigators

--- App.js
+++ App.js
@@ -11,12 +11,100 @@ import {
   Image,
   ScrollView,
   SafeAreaView,
+  Button,
 } from "react-native";
 import Screen from "./app/components/Screen";
 import ListingEditScreen from "./app/screens/ListingEditScreen";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+const Link = () => {
+  const navigation = useNavigation(); // for ChildComponents
+
+  return (
+    <Button
+      title="Click"
+      onPress={() => navigation.navigate("TweetDetails", { id: 1 })}
+    />
+  );
+};
+
+const Tweets = ({ navigation }) => (
+  <Screen>
+    <Text>Tweets</Text>
+    <Link />
+  </Screen>
+);
+const TweetDetails = ({ route }) => (
+  <Screen>
+    <Text>Tweet Details {route.params.id} </Text>
+  </Screen>
+);
+const Stack = createStackNavigator();
+const FeedNavigator = () => (
+  <Stack.Navigator
+    initialRouteName="Tweets"
+    screenOptions={{
+      headerStyle: { backgroundColor: "dodgerblue" },
+      headerTintColor: "white",
+      // applying styles globally
+    }}
+  >
+    <Stack.Screen
+      name="Tweets"
+      component={Tweets}
+      options={{
+        headerStyle: { backgroundColor: "tomato" },
+        headerTintColor: "white",
+        headerShown: false,
+      }} //  applying styles to each screen header locally
+    />
+    <Stack.Screen
+      name="TweetDetails"
+      options={({ route }) => ({ title: route.params.id })}
+      component={TweetDetails}
+    />
+  </Stack.Navigator>
+);
+
+const Account = () => (
+  <Screen>
+    <Text>Account</Text>
+  </Screen>
+);
+
+const Tab = createBottomTabNavigator();
+const TabNavigator = () => (
+  <Tab.Navigator
+    tabBarOptions={{
+      activeBackgroundColor: "tomato",
+      activeTintColor: "white",
+      inactiveBackgroundColor: "#eee",
+      inactiveTintColor: "black",
+    }}
+  >
+    <Tab.Screen
+      name="Feed"
+      options={{
+        tabBarIcon: ({ size, color }) => (
+          <MaterialCommunityIcons name="home" size={size} color={color} />
+        ),
+      }}
+      component={FeedNavigator}
+    />
+    <Tab.Screen name="Account" component={Account} />
+  </Tab.Navigator>
+);
 
 const App = () => {
-  return <ListingEditScreen />;
+  return (
+    <NavigationContainer>
+      {/* <StackNavigator /> */}
+      <TabNavigator />
+    </NavigationContainer>
+  );
 };
 
 export default App;
